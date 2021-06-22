74bea3343a68d2087aad896e64819cb279f29c56
ErnestC30 <chow_ernest06@hotmail.com>
2021-06-22 16:12:40 -0400

    Updated wikipedia scraper program

--- wikipedia_scraper.py
+++ wikipedia_scraper.py
@@ -1,15 +1,25 @@
 """
 Creating a way to scrape wikipedia articles using the 'requests' and 'beautifulsoup' library.
-Things to add:
-    Navigate through specificly given urls or access a random article
-    Count occurence of word
-    Get title of page
+Able to visit a random or selected Wikipedia page, then find occurences of specified words.
+Things that could be added:
+    More input checking
+    Use wikipedia search function to get a page
+    Find most common words in the page
 """
 
 import requests
 from bs4 import BeautifulSoup
 
-def open_page(url):
+def get_input():
+    #Returns user's input
+    print('')
+    print("Enter '1' to navigate to a random wikipedia page.")
+    print("Enter '2' to enter your own wikipedia url page.")
+    print("Enter '3' to find occurences of words on the current wikipedia page.")
+    print("Enter '4' to close the program.")
+    return input()
+
+def open_page(url='https://en.wikipedia.org/wiki/Special:Random'):
     #Returns a beautifulsoup object containing all html elements in the url.
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
@@ -19,12 +29,13 @@ def get_text(soup):
     #Returns all text in the beautifulsoup object.
     return soup.get_text(" ", strip=True)
 
-def get_header(soup):
-    #Returns the header of the page.
+def get_title(soup):
+    #Returns the title of the page.
     return soup.title.string[:-12]
     
-def find_occurence(text, *targets):
+def find_occurence(text, targets):
     #Returns the number of times each target word appears in the text as a list
+    #Targets taken as a list of strings
     words = text.split()
     count = {}
 
@@ -41,11 +52,41 @@ def find_occurence(text, *targets):
             print(f'The amount of times {target} appears in the text is: 0')
    
 def main():
-    pass
+    running = True
+    soup = ''
+    print("Welcome to the Wikipedia web scraper tool.")
+    while running:
+        selection = get_input()
 
-url = 'https://en.wikipedia.org/wiki/Special:Random'
-soup = open_page(url)
-text = get_text(soup)
-print(get_header(soup))
+        if selection == '1':
+            #Visit a random Wikipedia page.
+            soup = open_page()
+            print(f"The randomly selected Wikipedia page is the '{get_title(soup)}' page.")
+    
+        if selection == '2':
+            #Visit a specified Wikipedia page.
+            print("Enter a Wikipedia url link.")
+            url = input('')
+            soup = open_page(url)
+            print(f"Now visiting the '{get_title(soup)}' page.")
 
-#find_occurence(text, 'League', 'the', 'Rift')
+        if selection == '3':
+            #Find occurences of a list of words on the current Wikipedia page.
+            if not isinstance(soup, BeautifulSoup):
+                print("Need to go to a random or selected Wikipedia page first.")
+                continue
+
+            print(f"You are now accessing the '{get_title(soup)}' page.")
+            print('Enter words that you want to search for, separated by spaces.')
+            words = input()
+            targets = words.split()
+            find_occurence(soup.get_text(), targets)            
+
+        if selection == '4':
+            #Close program
+            running = False
+
+
+
+if __name__ == '__main__':
+    main()
